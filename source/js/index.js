4804bb7b7f449763cc9da8493790178175be0fb1
Ross Zurowski <ross@rosszurowski.com>
2015-11-14 09:30:55 -0500

    Clean up js

--- source/js/index.js
+++ source/js/index.js
@@ -8,63 +8,80 @@ import randf from 'randf'
 
 import Vector from './lib/vector'
 
-var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
-var even = num => Math.round(num / 2) * 2
+const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+const even = num => Math.round(num / 2) * 2
+const rgb = (r, g, b) => `rgb(${r}, ${g}, ${b})`
 
 ready(() => {
 
   document.body.classList.remove('loading')
 
-  var canvas = document.querySelector('.canvas')
-  var caption = document.querySelector('.caption')
-  var ctx = context({ canvas })
-  var app = loop(canvas, { scale: window.devicePixelRatio })
+  const canvas = document.querySelector('.canvas')
+  const caption = document.querySelector('.caption')
+  const ctx = context({ canvas })
+  const app = loop(canvas, { scale: window.devicePixelRatio })
 
-  var count = 150
-  var rate = 1 / 40
+  const count = 150
+  const rate = 1 / 40
+  const diff = 50
 
-  caption.style.width = `${even(window.innerHeight)}px`
+  const r = Math.min(app.shape[0], app.shape[1])
+  const points = []
 
-  var r = Math.min(app.shape[0], app.shape[1])
-  var diff = 50
-  var pts = []
-  var center = new Vector(app.shape[0]*0.65, app.shape[1]*0.5)
+  let [width, height] = app.shape
+  let scale = app.scale
+  let center = new Vector(app.shape[0]*0.65, app.shape[1]*0.5)
 
   for (let i = 0; i < count; i++) {
-    var p = Vector.create().random(randf(-r, r)).add(center)
+    var p = Vector.create()
+      .random(randf(-r, r))
+      .add(center)
     p.depth = randf(0.4, 0.8)
     p.scale = randf(0.5, 1.5)
-    p.center = center.clone().add({ x: randf(-diff, diff), y: randf(-diff, diff) })
-    pts.push(p)
+    p.center = center.clone().add({
+      x: randf(-diff, diff),
+      y: randf(-diff, diff)
+    })
+    points.push(p)
   }
+
+  caption.style.width = `${even(window.innerHeight)}px`
 
   app.on('tick', tick)
   app.on('resize', resize)
   if (!mobile.test(navigator.userAgent)) app.start()
 
-
   function tick () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    for (let i = 0; i < pts.length; i++) {
-      let pt = pts[i]
-      pt.depth += (randf(0.5, 1.0) - pt.depth) / 7
-      let color = Math.round(pt.depth * 255)
-      ctx.fillStyle = `rgb(${color}, ${color}, ${color})`
-      pt.rotate(pt.center, rate)
+    ctx.save()
+    ctx.scale(scale, scale)
+    ctx.clearRect(0, 0, width, height)
+    for (let i = 0; i < points.length; i++) {
+      const p = points[i]
+      p.depth += (randf(0.5, 1.0) - p.depth) / 7
+      const color = Math.round(p.depth * 255)
+      ctx.fillStyle = rgb(color, color, color)
+      p.rotate(p.center, rate)
       ctx.beginPath()
-      ctx.arc(pt.x, pt.y, pt.scale, 0, Math.PI * 2, true)
+      ctx.arc(p.x, p.y, p.scale, 0, Math.PI * 2, true)
       ctx.closePath()
       ctx.fill()
     }
+    ctx.restore()
   }
 
   function resize () {
-    center = new Vector(app.shape[0]/2, app.shape[1]/2)
+    [width, height] = app.shape
+    scale = app.scale
+    center = new Vector(width/2, height/2)
     caption.style.width = `${even(document.body.clientHeight)}px`
-    for (let i = 0; i < pts.length; i++) {
-      let pt = pts[i]
-      pt.center = center.clone().add({ x: randf(-diff, diff), y: randf(-diff, diff) })
-      pt.set(Vector.create().random(randf(-r, r)).add(pt.center))
+    for (let i = 0; i < points.length; i++) {
+      const p = points[i]
+      const position = Vector.create().random(randf(-r, r)).add(p.center)
+      p.center = center.clone().add({
+        x: randf(-diff, diff),
+        y: randf(-diff, diff)
+      })
+      p.set(position)
     }
   }
 
