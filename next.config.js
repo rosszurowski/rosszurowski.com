7c177997d91f95d194b1c77554320a9ab86b4986
Ross Zurowski <ross@rosszurowski.com>
2017-10-02 09:59:28 -0700

    Revert to Node 8.6

--- next.config.js
+++ next.config.js
@@ -1,22 +1,20 @@
 const fs = require('mz/fs');
-const assign = require('object.assign');
 
 const stripExtension = str => str.replace(/\.js$/, '');
 const pathsToPages = paths => paths.reduce(
-  (pages, path) => assign({}, pages, { [path]: { page: path } }),
+  (pages, path) => ({ ...pages, [path]: { page: path } }),
   {}
 );
 
-const getLogPaths = () => (
-  fs.readdir('./pages/log').then(entries => {
-    return entries
-      .map(stripExtension)
-      .map(path => `/log/${path}`);
-  })
-);
+const getLogPaths = async () => {
+  const entries = await fs.readdir('./pages/log');
+  return entries
+    .map(stripExtension)
+    .map(path => `/log/${path}`);
+}
 
 module.exports = {
-  exportPathMap () {
+  async exportPathMap () {
     const staticPaths = [
       '/',
       '/2017/japan',
@@ -24,11 +22,11 @@ module.exports = {
       '/_error',
     ];
 
-    return getLogPaths().then(logPaths => {
-      const paths = staticPaths.concat(logPaths);
-      const pathMap = pathsToPages(paths);
+    const logPaths = await getLogPaths();
 
-      return Promise.resolve(pathMap);
-    });
+    const paths = staticPaths.concat(logPaths);
+    const pathMap = pathsToPages(paths);
+
+    return pathMap;
   },
 };
