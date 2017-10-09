e078436e690cd6403ad63a0eee1dfb5c811a1d07
Ross Zurowski <ross@rosszurowski.com>
2017-10-09 09:47:08 -0700

    Update build process to support recursive posts

--- next.config.js
+++ next.config.js
@@ -1,24 +1,26 @@
-const fs = require('mz/fs');
+const readdir = require('recursive-readdir');
 
 const stripExtension = str => str.replace(/\.js$/, '');
+const stripPagesPath = str => str.replace(/^pages/, '');
 const pathsToPages = paths => paths.reduce((pages, path) => ({ ...pages, [path]: { page: path } }), {});
 
 const getLogPaths = async () => {
-  const entries = await fs.readdir('./pages/log');
-  return entries.map(stripExtension).map(path => `/log/${path}`);
+  const entries = await readdir('./pages/log');
+  return entries.map(stripExtension).map(stripPagesPath);
 };
 
 module.exports = {
   async exportPathMap() {
     const staticPaths = ['/', '/2017/japan', '/100'];
+
     const customPaths = {
       '/404.html': { page: '_redirect' },
     };
 
     const logPaths = await getLogPaths();
-
     const paths = staticPaths.concat(logPaths);
-    const pathMap = { ...customPaths, ...pathsToPages(paths) };
+
+    const pathMap = { ...pathsToPages(paths), ...customPaths };
 
     return pathMap;
   },
